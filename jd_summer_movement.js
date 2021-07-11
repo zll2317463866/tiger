01497b37b5818e412fb15549c268cca78b9b35d2
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-07-11 09:10:09 +0800

    Update jd_summer_movement.js

--- jd_summer_movement.js
+++ jd_summer_movement.js
@@ -9,7 +9,7 @@ https://raw.githubusercontent.com/smiek2221/scripts/master/MovementFaker.js
 
 */
 const $ = new Env('燃动夏季');
-const jd_MovementFaker = require('./jd_MovementFaker.js')
+const MovementFaker = require('./MovementFaker.js')
 // const MovementFaker = require('./ZooFaker.js')
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
