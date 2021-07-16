0aadc394d35edb7c23872716745922831aabdeb5
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-07-16 09:41:34 +0800

    Update jd_yqyl.js

--- jd_yqyl.js
+++ jd_yqyl.js
@@ -5,7 +5,7 @@
 
  #柠檬邀请有礼  
  #自定义邀请码环境变量
-export yqm="nnHBU2uZxYS%2FVWmE%2FJkdWhtY0ysGaLKiblvqb9lX1E4%3D"
+export yqm=""
 #柠檬邀请有礼
 [task_local]
 0 10 * * * http://nm66.top/jd_yqyl.js, tag=柠檬邀请有礼, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
@@ -17,7 +17,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
-let yqm = '';
+let yqm = 'nnHBU2uZxYS%2FVWmE%2FJkdWhtY0ysGaLKiblvqb9lX1E4%3D';
 let zdtx = false //设置为true自动抢提现100
 if (process.env.yqm) {
   yqm = process.env.yqm;
