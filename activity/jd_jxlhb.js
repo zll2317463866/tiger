12082e042aa9aed90407cadcebc2dfd2e24c30e6
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-07-28 09:34:09 +0800

    Update jd_jxlhb.js

--- activity/jd_jxlhb.js
+++ activity/jd_jxlhb.js
@@ -47,10 +47,10 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
   console.log('京喜领88元红包\n' +
       '活动入口：京喜app-》我的-》京喜领88元红包\n' +
       '助力逻辑：脚本会助力作者，介意请取消脚本')
- // let res = await getAuthorShareCode() || [];
- // let res2 = await getAuthorShareCode('https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/jxhb.json') || [];
- // if (res && res.activeId) $.activeId = res.activeId;
- // $.authorMyShareIds = [...((res && res.codes) || []), ...res2];
+  let res = await getAuthorShareCode() || [];
+  let res2 = await getAuthorShareCode('https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/jxhb.json') || [];
+  if (res && res.activeId) $.activeId = res.activeId;
+  $.authorMyShareIds = [...((res && res.codes) || []), ...res2];
   //开启红包,获取互助码
   for (let i = 0; i < cookiesArr.length; i++) {
     $.index = i + 1;
