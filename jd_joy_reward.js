38a2079eae402a7e28658239c1a5bb9db0d0365b
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-08-07 08:37:13 +0800

    Update jd_joy_reward.js

--- jd_joy_reward.js
+++ jd_joy_reward.js
@@ -9,14 +9,14 @@ Last Modified time: 2021-06-06 21:22:37
 ==============Quantumult X==============
 [task_local]
 #宠汪汪积分兑换奖品
-59 7,15,23 * * * jd_joy_reward.js, tag=宠汪汪积分兑换奖品, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdcww.png, enabled=true
+59 8,16,23 * * * jd_joy_reward.js, tag=宠汪汪积分兑换奖品, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdcww.png, enabled=true
 
 ==============Loon==============
 [Script]
-cron "59 7,15,23 * * *" script-path=jd_joy_reward.js,tag=宠汪汪积分兑换奖品
+cron "59 8,16,23 * * *" script-path=jd_joy_reward.js,tag=宠汪汪积分兑换奖品
 
 ================Surge===============
-宠汪汪积分兑换奖品 = type=cron,cronexp="59 7,15,23 * * *",wake-system=1,timeout=3600,script-path=jd_joy_reward.js
+宠汪汪积分兑换奖品 = type=cron,cronexp="59 8,16,23 * * *",wake-system=1,timeout=3600,script-path=jd_joy_reward.js
 
 ===============小火箭==========
 宠汪汪积分兑换奖品 = type=cron,script-path=jd_joy_reward.js, cronexpr="59 7,15,23 * * *", timeout=3600, enable=true
@@ -100,7 +100,7 @@ Date.prototype.Format = function (fmt) { //author: meizz
 async function joyReward() {
   try {
     let nowtime = new Date().Format("s.S")
-    let starttime = process.env.JOY_STARTTIME ? process.env.JOY_STARTTIME : 59;
+    let starttime = process.env.JOY_STARTTIME ? process.env.JOY_STARTTIME : 60;
     if(nowtime < 59) {
       let sleeptime = (starttime - nowtime) * 1000;
       console.log(`等待时间 ${sleeptime / 1000}`);
