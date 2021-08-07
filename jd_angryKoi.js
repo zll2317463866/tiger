aafbcebc765bc3c8324cecb340b4bd7e3db47af5
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-08-07 09:10:41 +0800

    Update jd_angryKoi.js

--- jd_angryKoi.js
+++ jd_angryKoi.js
@@ -1,21 +1,11 @@
 /*
-（需添加环境变量export kois="pt_pin1@pt_pin2"）
 愤怒的锦鲤
 更新时间：2021-7-11
 备注：高速并发请求，专治偷助力。在kois环境变量中填入需要助力的pt_pin，有多个请用@符号连接
 TG学习交流群：https://t.me/cdles
 0 0 * * * https://raw.githubusercontent.com/cdle/jd_study/main/jd_angryKoi.js
 */
-/*
-愤怒的锦鲤
-更新时间:2021-05-08
-活动入口：
-只支持Node.js支持N个京东账号
-脚本兼容: Node.js
-cron 1 7,12,19 * * * jd_beauty.js
- */
-const $ = new Env('愤怒的锦鲤');
-
+const $ = Env("愤怒的锦鲤")
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const ua = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randomString(40)}`
 var kois = process.env.kois ?? ""
