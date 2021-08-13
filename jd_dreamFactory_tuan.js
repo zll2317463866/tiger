00f67e65842d224ac88518414a1fd62f9b4136e1
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-08-13 10:44:00 +0800

    Update jd_dreamFactory_tuan.js

--- jd_dreamFactory_tuan.js
+++ jd_dreamFactory_tuan.js
@@ -1,8 +1,17 @@
 /*
 *京喜工厂开团
-
-1 0 * * * jd_dreamFactory_tuan.js
-
+*
+* cron 5,10,15,20 0 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/star_dreamFactory_tuan.js
+*
+*一个账号能参团一次，一个账号一天能开三次团，请根据自己的情况设置需要开团的CK，一般至少5个CK能成团
+*脚本每执行一次，会领取上一次成团的奖励和新开一次团，每天执行4次能开完3次团和领取3次团的奖励
+*环境变量：
+*   OPEN_DREAMFACTORY_TUAN 脚本默认第一个CK开团，例：若OPEN_DREAMFACTORY_TUAN="2,3"  则第2，第3个CK开团，其他账号参加第2，第3个CK开的团
+*助力规则：
+*   开团账号开团，其他账号自动参团。 例：有A,B,C账号，A，B账号开团，则B，C会参加A的团，A会参加B的团
+*   账号内互助之后，开团账号若有剩下参团次数，会尝试加入作者团
+*成团条件：
+*   成团所需人数根据活动所需人数变化，一般为5-7人，
 *   若5人成团，则5个CK能成团一次，9个CK能成团两次，13个CK能成团三次
 * */
 !function (t, r) { "object" == typeof exports ? module.exports = exports = r() : "function" == typeof define && define.amd ? define([], r) : t.CryptoJS = r() }(this, function () {
@@ -17,7 +26,7 @@ const JD_API_HOST = 'https://m.jingxi.com';
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const openTuanCK = $.isNode() ? (process.env.OPEN_DREAMFACTORY_TUAN ? process.env.OPEN_DREAMFACTORY_TUAN : '1'):'1';
-const helpFlag = false;//是否参考作者团
+const helpFlag = true;//是否参考作者团
 let tuanActiveId = ``;
 let cookiesArr = [], cookie = '', message = '';
 $.tuanIds = [];
@@ -103,29 +112,6 @@ if ($.isNode()) {
           await $.wait(2000);
           if($.tuanMax){$.tuanIds.shift();j--;}
         }
-      }
-    }
-  }
-  let res = [];
-  if(helpFlag){
-    res = await getAuthorShareCode('https://raw.githubusercontent.com/star261/jd/main/code/dreamFactory_tuan.json');
-    if(!res){
-      res = [];
-    }
-    if(res.length === 0){
-      return ;
-    }
-    console.log(`\n===============开始助力作者团===================`);
-    let thisTuanID = getRandomArrayElements(res, 1)[0];
-    $.tuanMax = false;
-    for (let i = 0; i < cookiesArr.length && !$.tuanMax; i++) {
-      if(openTuanCKList.includes((i+1).toString())){
-        $.index = i + 1;
-        cookie = cookiesArr[i];
-        $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
-        console.log(`账号${$.UserName} 去参加作者团： ${thisTuanID}`);
-        await JoinTuan(thisTuanID);
-        await $.wait(2000);
       }
     }
   }
