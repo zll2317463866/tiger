5eb76b52e60f486433642bce9d489fb5921765be
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-08-19 17:39:38 +0800

    Update jd_jddj_plantBeans.js

--- jd_jddj_plantBeans.js
+++ jd_jddj_plantBeans.js
@@ -3,8 +3,14 @@
 兼容京东jdCookie.js
 手机设备在boxjs里填写cookie
 boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json
-new Env('京东到家鲜豆庄园');
-cron "20 0-23/6 * * *" jd_jddj_plantBeans.js
+TG群:https://t.me/passerbyb2021
+
+[task_local]
+10 0 * * * https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_plantBeans.js
+
+[Script]
+cron "10 0 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_plantBeans.js,tag=京东到家鲜豆庄园
+
 */
 
 const $ = new API("jddj_plantBeans");
