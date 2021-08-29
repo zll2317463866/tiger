88df698d017f91e4164bcd53f83ce4da3f12b77b
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-08-29 10:11:43 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -31,7 +31,7 @@ let helpAuthor = true;
 const randomCount = $.isNode() ? 5 : 5;
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
-  `eU9YaeW1N_ki9Wndn3MV1Q@eU9YaOm2Mq1z8GaEnXFG0Q`,
+`eU9YaeW1N_ki9Wndn3MV1Q@eU9YaOm2Mq1z8GaEnXFG0Q`,
   `eU9YaeW1N_ki9Wndn3MV1Q@eU9YaOm2Mq1z8GaEnXFG0Q`,
 ]
 if ($.isNode()) {
@@ -447,10 +447,10 @@ function getSign(functionid, body, uuid) {
       "clientVersion":"10.1.0"
     }
     let options = {
-      url: `https://jdsign.cf/ddo`,
+      url: `https://cdn.jdsign.cf/ddo`,
       body: JSON.stringify(data),
       headers: {
-        "Host": "jdsign.tk",
+        "Host": "jdsign.cf",
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }
     }
@@ -490,7 +490,7 @@ function showMsg() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `https://code.chiang.fun/api/v1/jd/jdcash/read/${randomCount}/`, 'timeout': 10000}, (err, resp, data) => {
+    $.get({url: `http://code.chiang.fun/api/v1/jd/jdcash/read/${randomCount}/`, 'timeout': 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
