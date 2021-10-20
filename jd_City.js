04fcfb300728ef680e584ae0b4fb509de1d8f4dd
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-10-20 18:05:08 +0800

    Update jd_City.js

--- jd_City.js
+++ jd_City.js
@@ -98,19 +98,7 @@ let inviteCodes = []
             }
             break
         }
-        if (!$.canHelp) continue
-        for (let code of insertCodes) {
-            console.log(`\n【${$.UserName}】去助力【作者】邀请码：${code}`);
-            let res = await getInfo(code)
-            if (res && res['data'] && res['data']['bizCode'] === 0) {
-                if (res['data']['result']['toasts'] && res['data']['result']['toasts'][0] && res['data']['result']['toasts'][0]['status'] === '3') {
-                    console.log(`助力次数已耗尽，跳出`)
-                    break
-                }
-                if (res['data']['result']['toasts'] && res['data']['result']['toasts'][0]) {
-                    console.log(`助力 作者【${code}】:${res.data.result.toasts[0].msg}`)
-                }
-            }
+       
             if ((res && res['status'] && res['status'] === '3') || (res && res.data && res.data.bizCode === -11)) {
                 // 助力次数耗尽 || 黑号
                 break
