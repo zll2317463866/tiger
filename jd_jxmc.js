8b51498e07d185c6e04e4e085b0224008aecf5d4
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-11-03 16:53:19 +0800

    Update jd_jxmc.js

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -131,7 +131,7 @@ if ($.isNode()) {
 function getShareCode(name) {
   return new Promise(resolve => {
     $.get({
-      url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/"+name,
+      //url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/"+name,
       headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }
@@ -141,7 +141,7 @@ function getShareCode(name) {
           console.log(`${JSON.stringify(err)}`);
           console.log(`${$.name} API请求失败，请检查网路重试`);
         } else {
-          console.log(`优先账号内部互助，有剩余助力次数再帮【zero205】助力`);
+          console.log(`优先账号内部互助，有剩余助力次数不会帮【zero205】助力`);
           $.shareCode = JSON.parse(data);
         }
       } catch (e) {
