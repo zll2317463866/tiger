b42c4681319c4a437d878cd58a824961de4c2b41
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-11-03 16:42:38 +0800

    Update jd_jxmc.js

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -23,6 +23,8 @@ let cookiesArr = [],token = {},ua = '';
 $.appId = 10028;
 let activeid = 'null';
 $.inviteCodeList = [];
+$.inviteCodeList_rp = [];
+let flag_hb = true
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
         cookiesArr.push(jdCookieNode[item])
@@ -69,7 +71,34 @@ if ($.isNode()) {
         console.log('\n脚本早上9点到10点直接执行，才会执行账号内互助');
         return ;
     }
+    if (flag_hb) {
+        console.log('\n##################开始账号内互助(红包)#################\n');
+        await getShareCode('jxmc_hb.json')
+        $.inviteCodeList_rp = [...($.inviteCodeList_rp || []), ...($.shareCode || [])]
+        for (let j = 0; j < cookiesArr.length; j++) {
+            $.cookie = cookiesArr[j];
+            $.UserName = decodeURIComponent($.cookie.match(/pt_pin=(.+?);/) && $.cookie.match(/pt_pin=(.+?);/)[1]);
+            token = await getJxToken();
+            $.canHelp = true;
+            for (let k = 0; k < $.inviteCodeList_rp.length; k++) {
+                $.oneCodeInfo = $.inviteCodeList_rp[k];
+                activeid = $.oneCodeInfo.activeid;
+                if($.oneCodeInfo.use === $.UserName) continue;
+                if (!$.canHelp) break;
+                if($.oneCodeInfo.use === $.UserName){
+                    continue
+                }
+                console.log(`\n${$.UserName}去助力${$.oneCodeInfo.use},助力码：${$.oneCodeInfo.code}\n`);
+                let helpInfo = await takeRequest(`jxmc`,`operservice/InviteEnroll`,`&sharekey=${$.oneCodeInfo.code}`,`activeid%2Cactivekey%2Cchannel%2Cjxmc_jstoken%2Cphoneid%2Csceneid%2Csharekey%2Ctimestamp`,true);
+                // console.debug(helpInfo)
+                await $.wait(2000);
+            }
+        }
+    }
     console.log('\n##################开始账号内互助#################\n');
+    $.shareCode = undefined
+    await getShareCode('jxmc.json')
+    $.inviteCodeList = [...($.inviteCodeList || []), ...($.shareCode || [])]
     for (let j = 0; j < cookiesArr.length; j++) {
         $.cookie = cookiesArr[j];
         $.UserName = decodeURIComponent($.cookie.match(/pt_pin=(.+?);/) && $.cookie.match(/pt_pin=(.+?);/)[1]);
@@ -99,7 +128,48 @@ if ($.isNode()) {
         }
     }
 })().catch((e) => {$.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')}).finally(() => {$.done();})
-
+function getShareCode(name) {
+  return new Promise(resolve => {
+    $.get({
+      url: "https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/"+name,
+      headers: {
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
+      }
+    }, async (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`);
+          console.log(`${$.name} API请求失败，请检查网路重试`);
+        } else {
+          console.log(`优先账号内部互助，有剩余助力次数再帮【zero205】助力`);
+          $.shareCode = JSON.parse(data);
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  })
+}
+async function get_rp(){
+    let rpInfo = await takeRequest(`jxmc`,`operservice/GetInviteStatus`,``,``,true);
+    if (rpInfo.ret === 0) {
+        if(rpInfo.data.sharekey){
+            console.log(`红包邀请码:${rpInfo.data.sharekey}`);
+            $.inviteCodeList_rp.push({'use':$.UserName,'code':rpInfo.data.sharekey,'max':false,'activeid':activeid});
+        }
+    } else if(rpInfo.ret === 2704){
+        console.log('红包今天领完了,跳过红包相关')
+        flag_hb = false
+    } else if(rpInfo.ret === 2706){
+        console.log('此帐号红包助力已满')
+    } else if(rpInfo.ret === 1016){
+        console.log('此帐号红包火爆')
+    } else{
+        console.log(`未知异常：${JSON.stringify(rpInfo)}\n`);
+    }
+}
 async function main() {
     ua = '';
     if(JXUserAgent){
@@ -151,6 +221,9 @@ async function main() {
     if(!petidList || petidList.length === 0){
         console.log(`账号内没有小鸡，暂停执行`);
         return ;
+    }
+    if (flag_hb) {
+      await get_rp()
     }
     $.inviteCodeList.push({'use':$.UserName,'code':homePageInfo.sharekey,'max':false,'activeid':activeid});
     if(JSON.stringify(visitBackInfo) !== '{}'){
@@ -227,7 +300,56 @@ async function main() {
     await doMotion(petidList);
     await buyCabbage(homePageInfo);
     await feed();
+    await doUserLoveInfo();
 }
+
+async function doUserLoveInfo() {
+    console.log(`助农活动`);
+    let taskLiskInfo = await takeRequest(`newtasksys`, `newtasksys_front/GetUserTaskStatusList`, `&source=jxmc_zanaixin&bizCode=jxmc_zanaixin&dateType=2&showAreaTaskFlag=0&jxpp_wxapp_type=7`, `bizCode%2CdateType%2Cjxpp_wxapp_type%2CshowAreaTaskFlag%2Csource`, false);
+    let taskLisk = taskLiskInfo.userTaskStatusList;
+    for (let i = 0; i < taskLisk.length; i++) {
+        let oneTask = taskLisk[i];
+        if(oneTask.awardStatus === 1){
+            console.log(`任务：${oneTask.taskName},已完成`)
+            continue;
+        }
+        if (oneTask.awardStatus === 2 && oneTask.completedTimes === oneTask.targetTimes) {
+            console.log(`完成任务：${oneTask.taskName}`);
+            awardInfo = await takeRequest(`newtasksys`, `newtasksys_front/Award`, `source=jxmc_zanaixin&taskId=${oneTask.taskId}&bizCode=jxmc_zanaixin`, `bizCode%2Csource%2CtaskId`, true);
+            console.log(`领取爱心成功，获得${JSON.parse(awardInfo.prizeInfo).prizeInfo}`);
+            await $.wait(2000);
+            $.freshFlag = true;
+        }
+        if(oneTask.taskId === 2147 || oneTask.taskId === 2157 || oneTask.taskId === 2167 || oneTask.taskId === 2171){
+            console.log(`去做任务：${oneTask.description}，等待5S`);
+            awardInfo = await takeRequest(`newtasksys`,`newtasksys_front/DoTask`,`source=jxmc_zanaixin&taskId=${oneTask.taskId}&bizCode=jxmc_zanaixin&configExtra=`,`bizCode%2CconfigExtra%2Csource%2CtaskId`,false);
+            await $.wait(5500);
+            console.log(`完成任务：${oneTask.description}`);
+            awardInfo = await takeRequest(`newtasksys`,`newtasksys_front/Award`,`source=jxmc_zanaixin&taskId=${oneTask.taskId}&bizCode=jxmc_zanaixin`,`bizCode%2Csource%2CtaskId`,true);
+            console.log(`领取爱心成功，获得${JSON.parse(awardInfo.prizeInfo).prizeInfo}`);
+        }
+
+        if(oneTask.taskId === 2154 && oneTask.completedTimes !== 1){
+            console.log(`去做任务：${oneTask.description}，等待5S`);
+            awardInfo = await takeRequest(`jxmc`,`operservice/GetInviteStatus`,``,undefined,true);
+            await $.wait(5500);
+            console.log(`完成任务：${oneTask.description}`);
+            awardInfo = await takeRequest(`newtasksys`,`newtasksys_front/Award`,`source=jxmc_zanaixin&taskId=${oneTask.taskId}&bizCode=jxmc_zanaixin`,`bizCode%2Csource%2CtaskId`,true);
+            console.log(`领取爱心成功，获得${JSON.parse(awardInfo.prizeInfo).prizeInfo}`);
+        }
+    }
+    let userLoveInfo = await takeRequest(`jxmc`, `queryservice/GetUserLoveInfo`, ``, undefined, true);
+    let lovelevel = userLoveInfo.lovelevel;
+    for (let i = 0; i < lovelevel.length; i++) {
+        if(lovelevel[i].drawstatus === 1){
+            console.log(`抽取红包`);
+            let drawLoveHongBao =await takeRequest(`jxmc`, `operservice/DrawLoveHongBao`, `&lovevalue=${lovelevel[i].lovevalue}`, `activeid%2Cactivekey%2Cchannel%2Cjxmc_jstoken%2Clovevalue%2Cphoneid%2Csceneid%2Ctimestamp`, true);
+            console.log(`抽取结果：${JSON.stringify(drawLoveHongBao)}`);
+            await $.wait(3000);
+        }
+    }
+}
+
 async function buyChick(configInfo,homePageInfo,cardInfo){
     console.log(`现共有小鸡：${homePageInfo.petinfo.length}只,小鸡上限：6只`);
     if(homePageInfo.petinfo.length === 6){
@@ -500,7 +622,7 @@ async function takeRequest(type,functionId,info,stk='activeid%2Cactivekey%2Cchan
                 console.log(data);
                 $.logErr(e, resp)
             } finally {
-                if(functionId === 'operservice/Feed'){
+                if(functionId === 'operservice/Feed' || functionId === 'operservice/GetInviteStatus'){
                     resolve(data || {});
                 }else{
                     resolve(data.data || {});
