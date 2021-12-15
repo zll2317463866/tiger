7e61b3182fe55d40176fbf7aac784ba338b5a0eb
tiger <83000809+zll2317463866@users.noreply.github.com>
2021-12-15 16:58:27 +0800

    Update jd_super_redrain.js

--- jd_super_redrain.js
+++ jd_super_redrain.js
@@ -5,14 +5,14 @@
 ==============Quantumult X==============
 [task_local]
 #整点京豆雨
-0 * * * * https://raw.githubusercontent.com/msechen/jdrain/main/jd_redrain.js, tag=整点京豆雨, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+0 * * * * https://raw.githubusercontent.com/KingRan/JDJB/main/jd_redrain.js, tag=整点京豆雨, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 ==============Loon==============
 [Script]
-cron "0 * * * *" script-path=https://raw.githubusercontent.com/msechen/jdrain/main/jd_redrain.js,tag=整点京豆雨
+cron "0 * * * *" script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_redrain.js,tag=整点京豆雨
 ================Surge===============
-整点京豆雨 = type=cron,cronexp="0 * * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/msechen/jdrain/main/jd_redrain.js
+整点京豆雨 = type=cron,cronexp="0 * * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_redrain.js
 ===============小火箭==========
-整点京豆雨 = type=cron,script-path=https://raw.githubusercontent.com/msechen/jdrain/main/jd_redrain.js, cronexpr="0 * * * *", timeout=3600, enable=true
+整点京豆雨 = type=cron,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_redrain.js, cronexpr="0 * * * *", timeout=3600, enable=true
 */
 const $ = new Env('整点红包雨');
 const notify = $.isNode() ? require('./sendNotify') : '';
