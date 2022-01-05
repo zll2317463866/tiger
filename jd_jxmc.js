383baf92faba84e7852a01872ba33278d2fb0cfd
tiger <83000809+zll2317463866@users.noreply.github.com>
2022-01-05 14:42:56 +0800

    Update jd_jxmc.js

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -34,6 +34,13 @@ $.inviteCodeList = [];
 let cookiesArr = [];
 let UA, token, UAInfo = {}
 $.appId = 10028;
+function oc(fn, defaultVal) {//optioanl chaining
+  try {
+    return fn()
+  } catch (e) {
+    return undefined
+  }
+}
 let cardinfo = {
   "16": "小黄鸡",
   "17": "辣子鸡",
@@ -152,7 +159,7 @@ async function pasture() {
         console.log(`\n温馨提示：${$.UserName} 请先手动完成【新手指导任务】再运行脚本再运行脚本\n`);
         return;
       }
-      $.currentStep = $.homeInfo?.finishedtaskId
+      $.currentStep = oc(() => $.homeInfo.finishedtaskId)
       console.log(`打印新手流程进度：当前进度：${$.currentStep}，下一流程：${$.homeInfo.maintaskId}`)
       if ($.homeInfo.maintaskId !== "pause" || isNew($.currentStep)) {
         console.log(`开始初始化`)
@@ -161,7 +168,7 @@ async function pasture() {
         for (let i = 0; i < 20; i++) {
           if ($.DoMainTask.maintaskId !== "pause") {
             await $.wait(2000)
-            $.currentStep = $.DoMainTask?.finishedtaskId
+            $.currentStep = oc(() => $.DoMainTask.finishedtaskId)
             $.step = $.DoMainTask.maintaskId
             await takeGetRequest('DoMainTask');
           } else if (isNew($.currentStep)) {
@@ -181,14 +188,14 @@ async function pasture() {
         let vo = $.taskList[key]
         if (vo.taskName === "邀请好友助力养鸡" || vo.taskType === 4) {
           if (vo.completedTimes >= vo.configTargetTimes) {
-            //console.log(`助力已满，不上传助力码`)
+            //console.log(`助力已满`)
           } else {
             $.inviteCodeList.push($.homeInfo.sharekey);
             await $.wait(2000)
           }
         }
       }
-      const petNum = ($.homeInfo?.petinfo || []).length
+      const petNum = (oc(() => $.homeInfo.petinfo) || []).length
       await takeGetRequest('GetCardInfo');
       if ($.GetCardInfo && $.GetCardInfo.cardinfo) {
         let msg = '';
@@ -644,7 +651,7 @@ function dealReturn(type, data) {
         $.homeInfo = data.data;
         $.activeid = $.homeInfo.activeid
         $.activekey = $.homeInfo.activekey || null
-        $.coins = $.homeInfo?.coins || 0;
+        $.coins = oc(() => $.homeInfo.coins) || 0;
         if ($.homeInfo.giftcabbagevalue) {
           console.log(`登陆获得白菜：${$.homeInfo.giftcabbagevalue} 颗`);
         }
