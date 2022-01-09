1adaeaa396e7546b94f7f282800c56e80b964d06
tiger <83000809+zll2317463866@users.noreply.github.com>
2022-01-09 16:24:36 +0800

    Update jd_city.js

--- jd_city.js
+++ jd_city.js
@@ -30,7 +30,7 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 let inviteCodes = [
-  '-ryUXKULN2FHZxXMHtvD9FPaUUY_XeDG',
+  '-ryUXKULN2FHZxXMHtvD9FPaUUY_XeDG'
 ]
 $.shareCodesArr = [];
 
