017e5a97bdbb5327e98e6dfb25567696b58afc58
tiger <83000809+zll2317463866@users.noreply.github.com>
2022-02-07 11:30:33 +0800

    Update jd_hwyf.js

--- jd_hwyf.js
+++ jd_hwyf.js
@@ -1,11 +1,11 @@
 /*
 修改自Aaron-lv的许愿池
-
+虎娃迎福
 活动入口：京东-边赚边玩-虎娃迎福
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 ===============Quantumultx===============
 [task_local]
-#虎娃迎福-JOY的年味大挑战-新年宠粉
+#虎娃迎福
 40 0,2 * * * https://github.com/mrddddgggg/dongge/raw/main/jd_hwyf.js, tag=虎娃迎福, img-url=https://user-images.githubusercontent.com/94060932/150673806-66451600-5419-47f4-998a-3b5ad579a3f1.jpg, enabled=true
 
 ================Loon==============
@@ -18,7 +18,7 @@ cron "40 0,2 * * *" script-path=https://github.com/mrddddgggg/dongge/raw/main/jd
 ============小火箭=========
 虎娃迎福 = type=cron,script-path=https://github.com/mrddddgggg/dongge/raw/main/jd_hwyf.js, cronexpr="40 0,2 * * *", timeout=3600, enable=true
  */
-const $ = new Env('虎娃迎福-JOY的年味大挑战-新年宠粉');
+const $ = new Env('疯狂砸金蛋-虎娃迎福-JOY的年味大挑战-新年宠粉');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
@@ -26,8 +26,8 @@ let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ['1GVJWyqg','1GVFUx6g','1GFNRxq8'];
-let appNameArr = ['虎娃迎福','JOY的年味大挑战','新年宠粉'];
+let appIdArr = ['1EFRQwA','1GVJWyqg','1GVFUx6g','1GFNRxq8'];
+let appNameArr = ['疯狂砸金蛋','虎娃迎福','JOY的年味大挑战','新年宠粉'];
 let appId, appName;
 $.shareCode = [];
 if ($.isNode()) {
