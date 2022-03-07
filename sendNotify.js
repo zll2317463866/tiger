c1c368898f9b5198e041bb1ebccbc10843d2e42b
tiger <83000809+zll2317463866@users.noreply.github.com>
2022-03-07 15:40:59 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -14,7 +14,7 @@ const querystring = require('querystring');
 const exec = require('child_process').exec;
 const $ = new Env();
 const timeout = 15000; //超时时间(单位毫秒)
-console.log("加载sendNotify，当前版本: 20220128");
+console.log("加载sendNotify，当前版本: 20220306");
 // =======================================go-cqhttp通知设置区域===========================================
 //gobot_url 填写请求地址http://127.0.0.1/send_private_msg
 //gobot_token 填写在go-cqhttp文件设置的访问密钥
@@ -470,7 +470,9 @@ async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By cc
         const titleIndexGp5 = notifyGroup5List.findIndex((item) => item === strTitle);
         const notifyGroup6List = process.env.NOTIFY_GROUP6_LIST ? process.env.NOTIFY_GROUP6_LIST.split('&') : [];
         const titleIndexGp6 = notifyGroup6List.findIndex((item) => item === strTitle);
-
+		const notifyGroup7List = process.env.NOTIFY_GROUP7_LIST ? process.env.NOTIFY_GROUP7_LIST.split('&') : [];
+        const titleIndexGp7 = notifyGroup7List.findIndex((item) => item === strTitle);
+		
         if (titleIndex2 !== -1) {
             console.log(`${strTitle} 在群组2推送名单中，初始化群组推送`);
             UseGroupNotify = 2;
@@ -490,6 +492,10 @@ async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By cc
         if (titleIndexGp6 !== -1) {
             console.log(`${strTitle} 在群组6推送名单中，初始化群组推送`);
             UseGroupNotify = 6;
+        }
+		if (titleIndexGp7 !== -1) {
+            console.log(`${strTitle} 在群组7推送名单中，初始化群组推送`);
+            UseGroupNotify = 7;
         }
         if (process.env.NOTIFY_CUSTOMNOTIFY) {
             strCustom = process.env.NOTIFY_CUSTOMNOTIFY;
@@ -526,7 +532,10 @@ async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By cc
                             console.log("自定义设定强制使用组6配置通知...");
                             UseGroupNotify = 6;
                         }
-
+						if (strCustomTempArr[1] == "组7") {
+                            console.log("自定义设定强制使用组6配置通知...");
+                            UseGroupNotify = 7;
+                        }
                         if (strCustomTempArr.length > 2) {
                             console.log("关闭所有通知变量...");
                             Use_serverNotify = false;
@@ -1232,6 +1241,110 @@ async function sendNotify(text, desp, params = {}, author = '\n\n本通知 By cc
                 GOTIFY_PRIORITY = process.env.GOTIFY_PRIORITY6;
             }
             break;
+			
+		case 7:
+            //==========================第七套环境变量赋值=========================
+
+            if (process.env.GOBOT_URL7 && Use_gobotNotify) {
+                GOBOT_URL = process.env.GOBOT_URL7;
+            }
+            if (process.env.GOBOT_TOKEN7 && Use_gobotNotify) {
+                GOBOT_TOKEN = process.env.GOBOT_TOKEN7;
+            }
+            if (process.env.GOBOT_QQ7 && Use_gobotNotify) {
+                GOBOT_QQ = process.env.GOBOT_QQ7;
+            }
+
+            if (process.env.PUSH_KEY7 && Use_serverNotify) {
+                SCKEY = process.env.PUSH_KEY7;
+            }
+
+            if (process.env.WP_APP_TOKEN7 && Use_WxPusher) {
+                WP_APP_TOKEN = process.env.WP_APP_TOKEN7;
+            }
+
+            if (process.env.WP_TOPICIDS7 && Use_WxPusher) {
+                WP_TOPICIDS = process.env.WP_TOPICIDS7;
+            }
+
+            if (process.env.WP_UIDS7 && Use_WxPusher) {
+                WP_UIDS = process.env.WP_UIDS7;
+            }
+
+            if (process.env.WP_URL7 && Use_WxPusher) {
+                WP_URL = process.env.WP_URL7;
+            }
+            if (process.env.BARK_PUSH7 && Use_BarkNotify) {
+                if (process.env.BARK_PUSH7.indexOf('https') > -1 || process.env.BARK_PUSH7.indexOf('http') > -1) {
+                    //兼容BARK自建用户
+                    BARK_PUSH = process.env.BARK_PUSH7;
+                } else {
+                    BARK_PUSH = `https://api.day.app/${process.env.BARK_PUSH7}`;
+                }
+                if (process.env.BARK_SOUND7) {
+                    BARK_SOUND = process.env.BARK_SOUND7;
+                }
+                if (process.env.BARK_GROUP7) {
+                    BARK_GROUP = process.env.BARK_GROUP7;
+                }
+            }
+            if (process.env.TG_BOT_TOKEN7 && Use_tgBotNotify) {
+                TG_BOT_TOKEN = process.env.TG_BOT_TOKEN7;
+            }
+            if (process.env.TG_USER_ID7 && Use_tgBotNotify) {
+                TG_USER_ID = process.env.TG_USER_ID7;
+            }
+            if (process.env.TG_PROXY_AUTH7 && Use_tgBotNotify)
+                TG_PROXY_AUTH = process.env.TG_PROXY_AUTH7;
+            if (process.env.TG_PROXY_HOST7 && Use_tgBotNotify)
+                TG_PROXY_HOST = process.env.TG_PROXY_HOST7;
+            if (process.env.TG_PROXY_PORT7 && Use_tgBotNotify)
+                TG_PROXY_PORT = process.env.TG_PROXY_PORT7;
+            if (process.env.TG_API_HOST7 && Use_tgBotNotify)
+                TG_API_HOST = process.env.TG_API_HOST7;
+
+            if (process.env.DD_BOT_TOKEN7 && Use_ddBotNotify) {
+                DD_BOT_TOKEN = process.env.DD_BOT_TOKEN7;
+                if (process.env.DD_BOT_SECRET7) {
+                    DD_BOT_SECRET = process.env.DD_BOT_SECRET7;
+                }
+            }
+
+            if (process.env.QYWX_KEY7 && Use_qywxBotNotify) {
+                QYWX_KEY = process.env.QYWX_KEY7;
+            }
+
+            if (process.env.QYWX_AM7 && Use_qywxamNotify) {
+                QYWX_AM = process.env.QYWX_AM7;
+            }
+
+            if (process.env.IGOT_PUSH_KEY7 && Use_iGotNotify) {
+                IGOT_PUSH_KEY = process.env.IGOT_PUSH_KEY7;
+            }
+
+            if (process.env.PUSH_PLUS_TOKEN7 && Use_pushPlusNotify) {
+                PUSH_PLUS_TOKEN = process.env.PUSH_PLUS_TOKEN7;
+            }
+            if (process.env.PUSH_PLUS_USER7 && Use_pushPlusNotify) {
+                PUSH_PLUS_USER = process.env.PUSH_PLUS_USER7;
+            }
+
+            if (process.env.PUSH_PLUS_TOKEN_hxtrip7 && Use_pushPlushxtripNotify) {
+                PUSH_PLUS_TOKEN_hxtrip = process.env.PUSH_PLUS_TOKEN_hxtrip7;
+            }
+            if (process.env.PUSH_PLUS_USER_hxtrip7 && Use_pushPlushxtripNotify) {
+                PUSH_PLUS_USER_hxtrip = process.env.PUSH_PLUS_USER_hxtrip7;
+            }
+            if (process.env.GOTIFY_URL7) {
+                GOTIFY_URL = process.env.GOTIFY_URL7;
+            }
+            if (process.env.GOTIFY_TOKEN7) {
+                GOTIFY_TOKEN = process.env.GOTIFY_TOKEN7;
+            }
+            if (process.env.GOTIFY_PRIORITY7) {
+                GOTIFY_PRIORITY = process.env.GOTIFY_PRIORITY7;
+            }
+            break;
         }
 
         //检查是否在不使用Remark进行名称替换的名单
@@ -1474,7 +1587,7 @@ function getQLinfo(strCK, intcreated, strTimestamp, strRemark) {
     var strCheckCK = strCK.match(/pt_key=([^; ]+)(?=;?)/) && strCK.match(/pt_key=([^; ]+)(?=;?)/)[1];
     var strPtPin = decodeURIComponent(strCK.match(/pt_pin=([^; ]+)(?=;?)/) && strCK.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
     var strReturn = "";
-    if (strCheckCK.substring(0, 4) == "AAJh") {
+    if (strCheckCK.substring(0, 3) == "AAJ") {
         var DateCreated = new Date(intcreated);
         var DateTimestamp = new Date(strTimestamp);
         var DateToday = new Date();
@@ -1859,57 +1972,58 @@ function BarkNotify(text, desp, params = {}) {
 }
 
 function tgBotNotify(text, desp) {
-    return new Promise((resolve) => {
-        if (TG_BOT_TOKEN && TG_USER_ID) {
-            const options = {
-                url: `https://${TG_API_HOST}/bot${TG_BOT_TOKEN}/sendMessage`,
-                body: `chat_id=${TG_USER_ID}&text=${text}\n\n${desp}&disable_web_page_preview=true`,
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                timeout,
-            };
-            if (TG_PROXY_HOST && TG_PROXY_PORT) {
-                const tunnel = require('tunnel');
-                const agent = {
-                    https: tunnel.httpsOverHttp({
-                        proxy: {
-                            host: TG_PROXY_HOST,
-                            port: TG_PROXY_PORT * 1,
-                            proxyAuth: TG_PROXY_AUTH,
-                        },
-                    }),
-                };
-                Object.assign(options, {
-                    agent
-                });
+  return new Promise(resolve => {
+    if (TG_BOT_TOKEN && TG_USER_ID) {
+      const options = {
+        url: `https://${TG_API_HOST}/bot${TG_BOT_TOKEN}/sendMessage`,
+        json: {
+            chat_id: `${TG_USER_ID}`,
+            text: `${text}\n\n${desp}`,
+            disable_web_page_preview:true,
+          },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        timeout
+      }
+      if (TG_PROXY_HOST && TG_PROXY_PORT) {
+        const tunnel = require("tunnel");
+        const agent = {
+          https: tunnel.httpsOverHttp({
+            proxy: {
+              host: TG_PROXY_HOST,
+              port: TG_PROXY_PORT * 1,
+              proxyAuth: TG_PROXY_AUTH
             }
-            $.post(options, (err, resp, data) => {
-                try {
-                    if (err) {
-                        console.log('telegram发送通知消息失败！！\n');
-                        console.log(err);
-                    } else {
-                        data = JSON.parse(data);
-                        if (data.ok) {
-                            console.log('Telegram发送通知消息成功🎉。\n');
-                        } else if (data.error_code === 400) {
-                            console.log('请主动给bot发送一条消息并检查接收用户ID是否正确。\n');
-                        } else if (data.error_code === 401) {
-                            console.log('Telegram bot token 填写错误。\n');
-                        }
-                    }
-                } catch (e) {
-                    $.logErr(e, resp);
-                }
-                finally {
-                    resolve(data);
-                }
-            });
-        } else {
-            resolve();
+          })
         }
-    });
+        Object.assign(options, {agent})
+      }
+      $.post(options, (err, resp, data) => {
+        try {
+          if (err) {
+            console.log('telegram发送通知消息失败！！\n')
+            console.log(err);
+          } else {
+            data = JSON.parse(data);
+            if (data.ok) {
+              console.log('Telegram发送通知消息成功�。\n')
+            } else if (data.error_code === 400) {
+              console.log('请主动给bot发送一条消息并检查接收用户ID是否正确。\n')
+            } else if (data.error_code === 401) {
+              console.log('Telegram bot token 填写错误。\n')
+            }
+          }
+        } catch (e) {
+          $.logErr(e, resp);
+        } finally {
+          resolve(data);
+        }
+      })
+    } else {     
+      resolve()
+    }
+  })
 }
 
 function ddBotNotify(text, desp) {
