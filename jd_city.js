534d2f08bcba7e4f77a6360ef7df9a4220e7daf0
tiger <83000809+zll2317463866@users.noreply.github.com>
2022-12-30 14:46:01 +0800

    Update jd_city.js

--- jd_city.js
+++ jd_city.js
@@ -25,7 +25,7 @@ if ($.isNode()) {
     cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let inviteCodes = []
+let inviteCodes = ['VC4fQ6oMD3Z']
 $.shareCodesArr = [];
 
 !(async () => {
